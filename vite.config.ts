e9d6066e5a78c2d332d001c51583d07c3a0452ef
shubham11602152 <shubhammehta.333@gmail.com>
2025-09-15 23:08:43 +0530

    chore: update Vite configuration for improved project structure

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Vector/",
+  // base: "/Vector/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
