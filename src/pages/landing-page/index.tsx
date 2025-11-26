fd22d0a1aaa4755f4ef002b7ea6203216fd9f8b3
Shubham Mehta <shubhammehta.vc@flipkart.com>
2025-11-27 00:36:04 +0530

    feat: add About Us section to landing page

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import PricingSection from "./components/pricing-section";
 import ServicesSection from "./components/services-section/index";
 import useAppContext from "@/context/useAppContext";
+import AboutUsSection from "./components/about-us-section";
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -19,25 +20,28 @@ function LandingPage() {
   };
 
   return (
-    <VerticalLayout>
-      {/* Silk background covers header + hero */}
-      <div className="absolute inset-0 -z-10 w-full h-[700px] md:h-[700px] invert brightness-80 dark:invert-0 ">
-        <HeroBackground />
-      </div>
-      <section className="flex flex-col items-center justify-center min-h-[60vh] gap-8 py-12">
-        {/* Pass handler to HeroContent for Get Started button */}
-        <HeroContent onGetStarted={handleGetStarted} />
-      </section>
-      <ServicesSection />
-
-      <section className="flex flex-col items-center justify-center min-h-[60vh] gap-8 py-12">
-        {/* Testimonial Section (structure only, marquee effect next) */}
-        <section className="w-full mt-20">
-          <TestimonialSection />
+    <>
+      <VerticalLayout>
+        {/* Silk background covers header + hero */}
+        <div className="absolute inset-0 -z-10 w-full h-[700px] md:h-[700px] invert brightness-80 dark:invert-0 ">
+          <HeroBackground />
+        </div>
+        <section className="flex flex-col items-center justify-center min-h-[60vh] gap-8 py-12">
+          {/* Pass handler to HeroContent for Get Started button */}
+          <HeroContent onGetStarted={handleGetStarted} />
         </section>
-      </section>
-      <PricingSection />
-    </VerticalLayout>
+        <AboutUsSection />
+        <ServicesSection />
+
+        <section className="flex flex-col items-center justify-center min-h-[60vh] gap-8 py-12">
+          {/* Testimonial Section (structure only, marquee effect next) */}
+          <section className="w-full mt-20">
+            <TestimonialSection />
+          </section>
+        </section>
+        <PricingSection />
+      </VerticalLayout>
+    </>
   );
 }
 
